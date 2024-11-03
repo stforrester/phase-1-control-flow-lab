b118662eae1756c6577cbec6d731b70de8eb5455
Stuart Forrester <stforrester@gmail.com>
2024-11-03 17:58:51 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
+  if (rideDistance <= 400) {
+    return 'This one is on me!';
+  }
+  else if ((rideDistance > 400)&&(rideDistance <= 2000)) {
+    return 'That will be twenty bucks.';
+  }
+  else if ((rideDistance > 2000)&&(rideDistance <= 2500)) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(inputParam){
   // Write your code here!
+  return inputParam === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipGenerosity){
   // Write your code here!
+  switch (tipGenerosity) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
